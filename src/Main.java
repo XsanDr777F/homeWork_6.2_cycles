b4479fabeedd211a1fe27c32fed7255ccbca9bed
Александр <79691189430@yandex.ru>
2023-07-06 21:49:39 +0300

    homeWork done

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,131 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        task_1();
+        task_2();
+        task_3();
+        task_4();
+        task_5();
+        task_6();
+        task_7();
+        task_8();
+        task_8_1();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void task_1() {
+        System.out.println("Задача 1");
+        int salary = 15000;
+        int total = 0;
+        int i = 0;
+
+        while (total < 2_459_000) {
+            total = total + salary;
+            i++;
+
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
         }
+
+    }
+
+    public static void task_2() {
+        System.out.println("Задача 2");
+
+        int a = 0;
+
+        while (a < 10) {
+            a++;
+            System.out.print(a + " ");
+        }
+        System.out.println();
+        for (; a >= 1; a--) {
+            System.out.print(a + " ");
+        }
+    }
+
+    public static void task_3() {
+        System.out.println("Задача 3");
+
+        int people = 12_000_000;
+        int birth = 17_000;
+        int death = 8_000;
+
+        for (int i = 1; i <= 10; i++) {
+            people = people + birth - death;
+            System.out.println("Год " + i + ", численность населения составляет " + people);
+        }
+
+    }
+
+    public static void task_4() {
+        System.out.println("Задача 4");
+        int salary = 15000;
+        double percent = 0.07F;
+        int total = 0;
+        int i = 0;
+        for (; total <= 12_000_000; i++) {
+            salary = (int) (salary * percent + salary);
+            total = (int) (salary + total * percent);
+            System.out.println("Месяц " + i + ", сумма накоплений " + total);
+        }
+
+    }
+
+    public static void task_5() {
+        System.out.println("Задача 5");
+        int salary = 15000;
+        double percent = 0.07F;
+        int total = 0;
+        int i = 0;
+        for (; total <= 12_000_000; i++) {
+            salary = (int) (salary * percent + salary);
+            total = (int) (salary + total * percent);
+            if (i % 6 == 0)
+                System.out.println("Месяц " + i + ", сумма накоплений " + total);
+        }
+
+    }
+
+    public static void task_6() {
+        System.out.println("Задача 6");
+        int salary = 15000;
+        double percent = 0.07F;
+        int total = 0;
+        int i = 0;
+        for (; i <= 9 * 12; i++) {
+            salary = (int) (salary * percent + salary);
+            total = (int) (salary + total * percent);
+            if (i % 6 == 0)
+                System.out.println("Месяц " + i + ", сумма накоплений " + total);
+        }
+    }
+
+    public static void task_7() {
+        System.out.println("Задача 7");
+        int friday = 3;
+        for (; friday <= 31; friday += 7) {
+            System.out.println("Сегодня пятница, " + friday + " число. Необходимо подготовить отчет");
+        }
+
+    }
+
+    public static void task_8() {
+        System.out.println("Задача 8");
+        int year = 0;
+        for (; year <= 2100; year += 79) {
+            if (year>1800 && year <2100)
+            System.out.println(year);
+        }
+
+    }
+    public static void task_8_1() {
+        System.out.println("Задача 8(альтернатива)");
+        int year = 0;
+        int start = 2023-200;
+        int end = 2023+100;
+        for (; year <= end; year += 79) {
+            if (year>start && year <end)
+                System.out.println(year);
+        }
+
     }
 }
